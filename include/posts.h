dbb9ef6921ed226d258d97de44be7cb7d99d79a1
BrunoMartins11 <brunocmartins@live.com.pt>
2018-04-02 21:17:26 +0100

    atualizei o ficheiro

--- include/posts.h
+++ include/posts.h
@@ -2,6 +2,16 @@
 #define __POSTS_H__
 
 typedef struct posts *POST;
-
+POST make_post(long postID, char* postTitle, int postType, long parentID, long score, Date data, long answerCount, long commentCount, char** tags, long ownerID){
+char* get_title(POST p);
+long get_post(POST p);
+int get_post_type(POST p);
+long get_parentID(POST p);
+long get_score(POST p);
+Date get_date(POST p);
+long get_awnserC(POST p);
+long get_commentC(POST p);
+long get_owner(POST p);
+char** get_tags(POST p);
 
 #endif
