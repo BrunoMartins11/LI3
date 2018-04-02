ef0a44adec25f86206cc1901e055462a9223455b
BrunoMartins11 <brunocmartins@live.com.pt>
2018-04-02 19:42:23 +0100

    adicionei profile.h

---
+++ include/profile.h
@@ -0,0 +1,11 @@
+#ifndef __POFILE__
+#define __PROFILE__
+typedef struct profile* PROFILE;
+
+PROFILE make_profile(long id, long rep, char *name, char *aboutme)
+long get_id(PROFILE p);
+long get_rep(PROFILE p);
+char* get_name(PROFILE p);
+char* get_profile(PROFILE p); 
+void free_profile(PROFILE u);
+#endif
