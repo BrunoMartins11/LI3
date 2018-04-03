5b410def9d8142fa8f913a51b0adbc42dc754409
BrunoMartins11 <brunocmartins@live.com.pt>
2018-04-03 15:08:57 +0100

    adicionei gets e clean e corrigi tipos

--- src/lib/HashTableLoad.c
+++ src/lib/HashTableLoad.c
@@ -2,15 +2,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <glib.h>
+#include "posts.h"
+#include "profile.h"
 
 struct TCD_Community{
 	GHashTable *profile;
 	GHashTable *posts;
 };
 
-TCD_Community create_struct(){
-	TCD_Community m = malloc(sizeof TCD_Community);
+TAD_Community create_struct(){
+	TAD_Community m = malloc(sizeof TAD_Community);
 	m->profile = g_hash_table_new_full( g_int64_hash, g_int64_equal, free_profile, ); //falta o ultimo parametro
 	m->posts = g_hash_table_new_full( g_int64_hash, g_int64_equal, free_post, ); //falta o ultimo parametro
 	return m;
+}
+
+GHashTable get_profile_hash(TAD_Community m){
+	if(m) return m->profile;
+	return NULL;
+}
+
+GHashTable get_posts_hash(TAD_Community m){
+	if (m) return m->posts;
+	return NULL;
+}
+
+void clean_hash_table(TAD_Community m){
+	if(m){
+	free_post(m->posts);
+	free_post(m->profile);
+	free(m);
+	}
 }
