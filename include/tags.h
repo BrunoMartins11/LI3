44dd39c90395ebf5716eb471619e24d1e515cba6
BrunoMartins11 <brunocmartins@live.com.pt>
2018-04-05 15:53:22 +0100

    adicionado tags.h

---
+++ include/tags.h
@@ -0,0 +1,10 @@
+#ifndef __TAG__
+#define __TAG__
+
+typedef struct tag *TAG;
+TAG create_tag(char* name, long tagID);
+char* get_tag_name(TAG t);
+long  get_tagID(TAG t);
+void free_tag(void* t);
+
+#endif
