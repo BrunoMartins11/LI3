ae65c1e84c960a4396c3b3761ab01ae5205a3dd8
BrunoMartins11 <brunocmartins@live.com.pt>
2018-04-05 19:18:15 +0100

    criei hashTableLoad.h

---
+++ include/HashTableLoad.h
@@ -0,0 +1,6 @@
+GHashTable* get_profile_hash(TAD_community m);
+GHashTable* get_postDate_hash(TAD_community m);
+GHashTable* get_posts_hash(TAD_community m);
+GHashTable* get_tags_hash(TAD_community m);
+void clean_hash_table(TAD_community m);
+TAD_community create_struct();
