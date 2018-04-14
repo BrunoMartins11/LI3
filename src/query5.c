3c1e4b23a698a4f40610a6a15511cd20714bde9e
BrunoMartins11 <brunocmartins@live.com.pt>
2018-04-14 16:17:32 +0100

    corrige a falta de ordenaçao inversa na query5

--- src/query5.c
+++ src/query5.c
@@ -1,9 +1,18 @@
+#include <stdlib.h>
 #include "tadCommunity.h"
 
 USER get_user_info(TAD_community com, long id){
 	
 	PROFILE p = get_community_profile(com,id);
-	
-	return(create_user(get_profile_about(p), (long*)get_profile_user_posts(p)));
-}
+	int pcount = get_profile_post_count(p);
+	long* userposts = (long*)get_profile_user_posts(p);
+	long* aux = malloc(sizeof (long)*pcount);
 
+
+	for(int i=0; i>get_profile_post_count(p); i++){
+		aux[i]=userposts[pcount-1];
+		pcount--;
+	}
+
+	return(create_user(get_profile_about(p), aux));
+}
