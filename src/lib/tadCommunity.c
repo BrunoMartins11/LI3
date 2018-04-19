7e109b9ba81e8e112b0ebc0eff662f5b6aaf2d4e
dunnadan <marcio.sousa98@hotmail.com>
2018-04-19 13:59:58 +0100

    Altera minimos detalhes de identaçao

--- src/lib/tadCommunity.c
+++ src/lib/tadCommunity.c
@@ -11,15 +11,18 @@
 #include "tadCommunity.h"
 
 struct _dynamic_list{
+	
 	GList *list;
 };
 
 dynamic_list* create_dynamic_list(){
+	
 	dynamic_list *l = malloc(sizeof (struct _dynamic_list));
 	return l;
 }
 
 struct TCD_community{
+	
 	GHashTable *profile;
 	GHashTable *posts;
 	GHashTable *tags;
@@ -28,11 +31,13 @@ struct TCD_community{
 
 
 TAD_community init(){
+	
 	return create_struct();
 }
 
 
 TAD_community create_struct(){
+	
 	TAD_community m = malloc(sizeof (struct TCD_community));
 	m->profile = g_hash_table_new_full( g_int64_hash, g_int64_equal, g_free,free_profile); 
 	m->posts = g_hash_table_new_full( g_int64_hash, g_int64_equal, g_free,free_post); 
@@ -53,8 +58,10 @@ void add_community_post(TAD_community m, long id, POST p){
 	
 	long* idp = malloc(sizeof(long));
 	*idp = get_post_id(p);
+	
 	long owner = get_post_owner(p);
 	PROFILE po = get_community_profile(m, owner);
+	
 	if(po) { 
 		increment_profile_post_count(po);
 		add_profile_post(get_community_profile(m, owner), id);
@@ -101,6 +108,7 @@ void iterate_community_users(TAD_community m, GHFunc func, gpointer data){
 }
 
 void clean_TAD_community(TAD_community m){
+	
 	if(m){
 		g_hash_table_destroy(m->profile);
 		g_hash_table_destroy(m->posts);
@@ -118,30 +126,39 @@ int str_in_postTitle(POST p, char* str){
 }
 
 GList* get_all_keys(GHashTable* dict){
+	
 	return g_hash_table_get_values(dict);
 }
 
 dynamic_list* get_all_keys_post(TAD_community m){
+	
 	dynamic_list *l = create_dynamic_list();
 	l->list = get_all_keys(m->posts);
+	
 	return l;
 }
 
 int list_size(dynamic_list* l){
+	
 	return (int) g_list_length(l->list);
 }
 
 dynamic_list* sort_list(dynamic_list* l, long func){
+	
 	l->list = g_list_sort(l->list, (GCompareFunc) func);
+	
 	return l;
 }
 
 long get_data_from_list(dynamic_list* l){
+	
 	return (long) l->list->data;
 }
 
 dynamic_list* next(dynamic_list* lista){
+	
 	lista->list = lista->list->next;
+	
 	return lista;
 }
 
