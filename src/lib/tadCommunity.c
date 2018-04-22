b984a74d09c0d44128ce3d647afa3ef4ee3a38b0
Leonardo Neri <leonardo.lneri@gmail.com>
2018-04-22 01:45:32 +0100

    Query 8 funcionando!

--- src/lib/tadCommunity.c
+++ src/lib/tadCommunity.c
@@ -131,6 +131,8 @@ int str_in_postTitle(POST p, char* str){
 }
 
 void add_questions_to_array(gpointer key, gpointer value, gpointer user_data){
-	if(get_post_type(value) == 1)
-		append_listG(user_data, key);
+	if(get_post_type(value) == 1){
+		long id = get_post_id(value);
+		append_listG(user_data, id);
+	}
 }
--- src/query8.c
+++ src/query8.c
@@ -6,6 +6,7 @@
 #include "posts.h"
 #include "tadCommunity.h"
 #include "dArray.h"
+#include "date.h"
 
 // Dado uma palavra, devolver uma lista com os IDs de N perguntas cujos tıtulos a contenham, ordenados por cronologia inversa;
 
@@ -16,18 +17,24 @@ LONG_list contains_word(TAD_community com, char* word, int N){
 
 	POST p;
 	LONG_list last_n;
+
+	//Criação da lista, adição de elementos, ordenação e obtenção do tamanho.
 	LISTG keys = create_listG(sizeof(long));
 	iterate_community_posts(com, add_questions_to_array, keys);
 	sort_listG(keys, listG_reverse_sort);
 	keys_size = listG_size(keys);
+
 	last_n = create_list(keys_size);
 
 
 	for(i = 0; i < keys_size && index < N; i++){
 		
+		//elemento da lista no index i
 		id = get_listG_index(keys, long, i);
+
 		p = get_community_post(com, id);
 		
+		//retorna true se a palavra word está no título do post
 		if(str_in_postTitle(p, word)){
 
 			set_list(last_n, index, get_post_id(p));
