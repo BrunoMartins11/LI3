e067b64265c2790b3b99d45f1244ee27d96162a4
dunnadan <marcio.sousa98@hotmail.com>
2018-04-24 15:28:40 +0100

    Apaga funcoes a mais no posts.h

--- include/posts.h
+++ include/posts.h
@@ -40,26 +40,6 @@ POST make_post(long postID,
  */
 char* get_post_title(POST p);
 
-long get_post_id(POST p);
-
-int get_post_type(POST p);
-
-long get_post_parentID(POST p);
-
-long get_post_score(POST p);
-
-Date get_post_date(POST p);
-
-long get_post_awnser_count(POST p);
-
-long get_post_comment_count(POST p);
-
-long get_post_owner(POST p);
-
-char** get_post_tags(POST p);
-
-void free_post(void* p);
-
 void add_answer(POST p, long id);
 
 GArray* get_answers(POST p);
