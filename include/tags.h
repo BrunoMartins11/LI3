f30ac043a3b94666da6326076883e8ee42c3078a
dunnadan <marcio.sousa98@hotmail.com>
2018-04-25 19:49:03 +0100

    Tags.h

--- include/tags.h
+++ include/tags.h
@@ -1,10 +1,14 @@
-#ifndef __TAG__
-#define __TAG__
+#ifndef __TAG_H__
+#define __TAG_H__
 
 typedef struct tag *TAG;
+
 TAG create_tag(char* name, long tagID);
+
 char* get_tag_name(TAG t);
+
 long  get_tag_id(TAG t);
+
 void free_tag(void* t);
 
 #endif
--- src/main.c
+++ src/main.c
@@ -15,8 +15,8 @@ int main(){
   LONG_list l = top_most_active(com, 10);
   //USER u = get_user_info(com, 15811);
   //LONG_list h = both_participated(com, 87, 5691, 10);
-  //for(int t=0; t<10; t++)
-    //printf("%ld\n", get_list(h,t));
+  //for(int t=0; t<100; t++)
+  //  printf("%ld\n", get_list(l,t));
   //STR_pair x = info_from_post(com, 796430); 
   //LONG_list l = top_most_active(com, 10);
   //USER u = get_user_info(com, 1550);
@@ -30,9 +30,9 @@ int main(){
 
   //LONG_pair p = total_posts(com, begin, end);
   //printf("%ld, %ld\n", get_fst_long(p),get_snd_long(p));
-  LONG_list h = most_voted_answers(com, 5, begin, end);
-  for(int t=0; t<10; t++)
-  printf("%ld\n", get_list(h,t));
+  //LONG_list h = most_voted_answers(com, 5, begin, end);
+  //for(int t=0; t<10; t++)
+  //printf("%ld\n", get_list(h,t));
   //long thing = better_answer(com,30334);
   //printf("%ld\n", thing);
   clean(com);
