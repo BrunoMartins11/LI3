48c3eaf3d51346942161946e65ff3bc10bd214f6
Leonardo Neri <leonardo.lneri@gmail.com>
2018-06-12 19:19:07 +0100

    doc Pair

--- proj-java/src/main/java/common/Pair.java
+++ proj-java/src/main/java/common/Pair.java
@@ -3,14 +3,32 @@ package common;
 import java.util.Objects;
 
 public class Pair<X,Y> {
+
+    /**
+     * Primeira componente
+     */
     private X fst;
+
+    /**
+     * Segunda componente
+     */
     private Y snd;
 
+
+    /**
+     * Contrutor parametrizado
+     * @param fst Valor da primeira componente
+     * @param snd Valor da segunda componente
+     */
     public Pair(X fst, Y snd) {
         this.fst = fst;
         this.snd = snd;
     }
 
+    /**
+     * Hash code do par
+     * @return Hash code do par
+     */
     public int hashCode() {
         int hashFst = fst != null ? fst.hashCode() : 0;
         int hashSnd = snd != null ? snd.hashCode() : 0;
@@ -18,6 +36,11 @@ public class Pair<X,Y> {
         return (hashFst + hashSnd) * hashSnd + hashFst;
     }
 
+    /**
+     * Método equals
+     * @param o objeto a ser comparado com o par
+     * @return true se o objeto é igual ao par, false caso contrário
+     */
     public boolean equals(Object o) {
         if (o instanceof Pair) {
             Pair p = (Pair) o;
@@ -26,14 +49,26 @@ public class Pair<X,Y> {
         return false;
     }
 
+    /**
+     * Converte o par em uma string
+     * @return String do par
+     */
     public String toString() {
         return "(" + fst + ", " + snd + ")";
     }
 
+    /**
+     * Valor da primeira componente
+     * @return Valor da primeira componente
+     */
     public X getFst() {
         return fst;
     }
 
+    /**
+     * Valor da segunda componente
+     * @return Valor da segunda componente
+     */
     public Y getSnd() {
         return snd;
     }
